21d7a6516cb9f21b03b81d09509afd141776f484
Jun Okada <jun@mtwo.co.jp>
2017-04-11 21:41:58 +0900

    tableの一部処理をプラグイン化。 ksupportプロジェクトをVS2015化。

---
+++ plugins/ksupport/table.cpp
@@ -0,0 +1,57 @@
+#include "ncbind.hpp"
+
+tjs_uint32 posHint, sizeHint, addHint;
+
+//----------------------------------------------------------------------
+// 配列を作成
+static tTJSVariant createArray(void)
+{
+  iTJSDispatch2 *obj = TJSCreateArrayObject();
+  tTJSVariant result(obj, obj);
+  obj->Release();
+  return result;
+}
+
+
+tTJSVariant table_find_list_range(tTJSVariant list, tjs_int pos, tjs_int size)
+{
+  ncbPropAccessor listObj(list);
+
+  tjs_int begin, end, mid, listCount;
+  begin = 0;
+  end = listCount = listObj.GetArrayCount();
+
+  tTJSVariant result = createArray();
+
+  while (begin < end) {
+    mid = int((begin + end) / 2);
+    tTJSVariant item = listObj.GetValue(mid, ncbTypedefs::Tag<tTJSVariant>());
+    ncbPropAccessor itemObj(item);
+    tjs_int itemPos = itemObj.GetValue(L"pos", ncbTypedefs::Tag<tjs_int>(), 8, &posHint);
+    tjs_int itemSize = itemObj.GetValue(L"size", ncbTypedefs::Tag<tjs_int>(), 8, &sizeHint);
+
+    if (itemPos <= pos && pos < itemPos + itemSize) {
+      ncbPropAccessor resultObj(result);
+      resultObj.FuncCall(0, L"add", &addHint, NULL, item);
+      for (tjs_int i = mid + 1; i < listCount; i++) {
+        tTJSVariant aItem = listObj.GetValue(i, ncbTypedefs::Tag<tTJSVariant>());
+        ncbPropAccessor aItemObj(aItem);
+        tjs_int aItemPos = aItemObj.GetValue(L"pos", ncbTypedefs::Tag<tjs_int>(), 8, &posHint);
+        if (aItemPos >= pos + size)
+          break;
+        resultObj.FuncCall(0, L"add", &addHint, NULL, aItem);
+      }
+      return result;
+    }
+    if (pos < itemPos)
+      end = mid;
+    else
+      begin = mid + 1;
+  }
+
+  return result;
+}
+
+//----------------------------------------------------------------------
+// バインド
+NCB_REGISTER_FUNCTION(table_find_list_range, table_find_list_range);
