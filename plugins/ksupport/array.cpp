37fd1fad3188a3263fcc10a24fb55095008cc849
Okada Jun <jun@mtwo.co.jp>
2024-11-28 13:22:53 +0900

    Array拡張の any, all が追加引数を受け取れるようにした。 Array拡張に以下の関数を追加した。 ・Array.flatten() 入れ子になった配列を展開する ・Array.findIf() 関数を渡してマッチするインデックスを検索する。 ・Array.slice() 配列の一部を切り出したサブ配列を返す。

--- plugins/ksupport/array.cpp
+++ plugins/ksupport/array.cpp
@@ -1,6 +1,7 @@
 #define NOMINMAX
 #include <windows.h>
 #include <vector>
+#include <stack>
 #include "tp_stub.h"
 #include "ncbind.hpp"
 
@@ -311,9 +312,9 @@ public:
 		return TJS_S_OK;
 	}
 
-	bool all(tTJSVariant val) {
-		ncbPropAccessor arrayObj(mObjthis);
-		auto arrayObjCount = count();
+	static bool __all__(tTJSVariant array, tTJSVariant val, std::vector<tTJSVariant*> &args) {
+		ncbPropAccessor arrayObj(array);
+		auto arrayObjCount = countArray(array);
 
 		tTJSVariant result = createArray();
 		ncbPropAccessor resultObj(result);
@@ -322,12 +323,11 @@ public:
 			auto &closure = val.AsObjectClosureNoAddRef();
 
 			if (closure.IsInstanceOf(0, NULL, NULL, L"Function", NULL) == TJS_S_TRUE) {
-				tTJSVariant *params[1];
 				for (tjs_uint i = 0; i < arrayObjCount; i++) {
 					tTJSVariant elm = arrayObj.GetValue(i, ncbTypedefs::Tag<tTJSVariant>());
 					tTJSVariant funcResult;
-					params[0] = &elm;
-					closure.FuncCall(0, NULL, NULL, &funcResult, 1, params, NULL);
+					args[0] = &elm;
+					closure.FuncCall(0, NULL, NULL, &funcResult, tjs_int(args.size()), &args[0], NULL);
 					if (! tjs_int(funcResult))
 						return false;
 				}
@@ -344,9 +344,21 @@ public:
 		return true;
 	}
 
-	bool any(tTJSVariant val) {
-		ncbPropAccessor arrayObj(mObjthis);
-		auto arrayObjCount = count();
+	static tjs_error TJS_INTF_METHOD all(tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis) {
+		if (numparams < 1)
+			return TJS_E_BADPARAMCOUNT;
+		tTJSVariant val = *param[numparams - 1];
+		std::vector<tTJSVariant*> args(1);
+		for (tjs_int i = 0; i < numparams - 1; i++)
+			args.push_back(param[i]);
+		if (result)
+			*result = __all__(tTJSVariant(objthis, objthis), val, args);
+		return TJS_S_OK;
+	}
+
+	static bool __any__(tTJSVariant array, tTJSVariant val, std::vector<tTJSVariant*> &args) {
+		ncbPropAccessor arrayObj(array);
+		auto arrayObjCount = countArray(array);
 
 		tTJSVariant result = createArray();
 		ncbPropAccessor resultObj(result);
@@ -355,12 +367,11 @@ public:
 			auto &closure = val.AsObjectClosureNoAddRef();
 
 			if (closure.IsInstanceOf(0, NULL, NULL, L"Function", NULL) == TJS_S_TRUE) {
-				tTJSVariant *params[1];
 				for (tjs_uint i = 0; i < arrayObjCount; i++) {
 					tTJSVariant elm = arrayObj.GetValue(i, ncbTypedefs::Tag<tTJSVariant>());
 					tTJSVariant funcResult;
-					params[0] = &elm;
-					closure.FuncCall(0, NULL, NULL, &funcResult, 1, params, NULL);
+					args[0] = &elm;
+					closure.FuncCall(0, NULL, NULL, &funcResult, tjs_int(args.size()), &args[0], NULL);
 					if (tjs_int(funcResult))
 						return true;
 				}
@@ -375,6 +386,18 @@ public:
 		}
 
 		return false;
+	}
+
+	static tjs_error TJS_INTF_METHOD any(tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis) {
+		if (numparams < 1)
+			return TJS_E_BADPARAMCOUNT;
+		tTJSVariant val = *param[numparams - 1];
+		std::vector<tTJSVariant*> args(1);
+		for (tjs_int i = 0; i < numparams - 1; i++)
+			args.push_back(param[i]);
+		if (result)
+			*result = __any__(tTJSVariant(objthis, objthis), val, args);
+		return TJS_S_OK;
 	}
 
 	static tTJSVariant __sort(tTJSVariant array, tTJSVariant cond, bool stable) {
@@ -459,6 +482,121 @@ public:
 
 		return result;
 	}
+
+	static tjs_int __findIf__(tTJSVariant array, tTJSVariant func, std::vector<tTJSVariant*> &args) {
+		ncbPropAccessor arrayObj(array);
+		auto arrayObjCount = countArray(array);
+
+		auto &funcClosure = func.AsObjectClosureNoAddRef();
+
+		for (tjs_uint i = 0; i < arrayObjCount; i++) {
+			tTJSVariant elm = arrayObj.GetValue(i, ncbTypedefs::Tag<tTJSVariant>());
+			tTJSVariant funcResult;
+			args[0] = &elm;
+			funcClosure.FuncCall(0, NULL, NULL, &funcResult, tjs_int(args.size()), &args[0], NULL);
+			if (tjs_int(funcResult))
+				return tjs_int(i);
+		}
+
+		return tjs_int(-1);
+	}
+
+	static tjs_error TJS_INTF_METHOD findIf(tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis) {
+		if (numparams < 1)
+			return TJS_E_BADPARAMCOUNT;
+
+		tTJSVariant func = *param[numparams - 1];
+		std::vector<tTJSVariant*> args(1);
+		for (tjs_int i = 0; i < numparams - 1; i++)
+			args.push_back(param[i]);
+
+		if (result)
+			*result = __findIf__(tTJSVariant(objthis, objthis), func, args);
+
+		return TJS_S_OK;
+	}
+
+	static tTJSVariant __slice__(tTJSVariant array, tjs_int startIndex, tjs_int size) {
+		ncbPropAccessor arrayObj(array);
+		auto arrayObjCount = countArray(array);
+
+		tTJSVariant result = createArray();
+		ncbPropAccessor resultObj(result);
+
+		auto endIndex = (size == 0) ? arrayObjCount : std::min(tjs_uint(startIndex + size), arrayObjCount);
+
+		for (tjs_uint i = startIndex; i < endIndex; i++) {
+			auto value = arrayObj.GetValue(i, ncbTypedefs::Tag<tTJSVariant>());
+			resultObj.SetValue(i - startIndex, value);
+		}
+
+		return result;
+	}
+
+	static tjs_error TJS_INTF_METHOD slice(tTJSVariant *result, tjs_int numparams, tTJSVariant **param, iTJSDispatch2 *objthis) {
+		if (numparams > 2)
+			return TJS_E_BADPARAMCOUNT;
+
+		tjs_int startIndex = *param[0];
+		tjs_int size = (numparams >= 2) ? *param[1] : 0;
+
+		if (result)
+			*result = __slice__(tTJSVariant(objthis, objthis), startIndex, size);
+
+		return TJS_S_OK;
+	}
+
+	tTJSVariant flatten() {
+		auto result = createArray();
+		ncbPropAccessor resultObj(result);
+
+		struct ArrayTravel {
+			ArrayTravel(iTJSDispatch2* array,
+						 tjs_uint count,
+						 tjs_uint index)
+				: array(array), count(count), index(index) {
+			}
+
+			iTJSDispatch2* array;
+			tjs_uint count;
+			tjs_uint index;
+		};
+		std::stack<ArrayTravel> arrayTravelStack;
+
+		arrayTravelStack.push(ArrayTravel(mObjthis, count(), 0));
+
+		iTJSDispatch2 *current;
+		tjs_uint count = 0;
+		tjs_uint index = 0;
+
+		for(;;) {
+			if (index >= count) {
+				if (arrayTravelStack.empty())
+					break;
+				auto &fs = arrayTravelStack.top();
+				current = fs.array;
+				count = fs.count;
+				index = fs.index;
+				arrayTravelStack.pop();
+				continue;
+			}
+			tTJSVariant elm;
+			current->PropGetByNum(0, index, &elm, current);
+			if (elm.Type() == tvtObject) {
+				auto elmDispatch = elm.AsObjectNoAddRef();
+				if (elmDispatch->IsInstanceOf(0, NULL, NULL, L"Array", elmDispatch) == TJS_S_TRUE) {
+					arrayTravelStack.push(ArrayTravel(current, count, index + 1));
+					current = elmDispatch;
+					count = countArray(elm);
+					index = 0;
+					continue;
+				}
+			}
+			resultObj.FuncCall(0, L"add", &addHint, NULL, elm);
+			index++;
+		}
+		return result;
+	}
 };
 
 
@@ -483,8 +621,8 @@ NCB_ATTACH_CLASS_WITH_HOOK(ArraySupport, Array) {
 	NCB_METHOD_RAW_CALLBACK(filterMap, ArraySupport::filterMap, 0);
 	NCB_METHOD_RAW_CALLBACK(select, ArraySupport::select, 0);
 	NCB_METHOD_RAW_CALLBACK(reject, ArraySupport::reject, 0);
-	NCB_METHOD(all);
-	NCB_METHOD(any);
+	NCB_METHOD_RAW_CALLBACK(all, ArraySupport::all, 0);
+	NCB_METHOD_RAW_CALLBACK(any, ArraySupport::any, 0);
 	NCB_METHOD(min);
 	NCB_METHOD(max);
 	NCB_METHOD_RAW_CALLBACK(_map, ArraySupport::_map, 0);
@@ -494,5 +632,8 @@ NCB_ATTACH_CLASS_WITH_HOOK(ArraySupport, Array) {
 	NCB_METHOD(inject);
 	NCB_METHOD_RAW_CALLBACK(_sort, ArraySupport::_sort, 0);
 	NCB_METHOD_RAW_CALLBACK(_reverse, ArraySupport::_reverse, 0);
+	NCB_METHOD_RAW_CALLBACK(findIf, ArraySupport::findIf, 0);
+	NCB_METHOD_RAW_CALLBACK(slice, ArraySupport::slice, 0);
+	NCB_METHOD(flatten);
 }
 
